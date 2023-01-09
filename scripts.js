6a5f8c710765e79d933734caa0373b5f66e5c7f0
mrlrcn <116755233+mrlrcn@users.noreply.github.com>
2023-01-09 14:26:20 +0100

    Fixed IF and added scripts

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// FUNCION TOGGLE //
+function toggle() {
+    var x = document.getElementById("js-showHide");
+    
+    if(x.style.display === "none")
+   {
+     x.style.display = "block";
+   }
+  else {
+    x.style.display = "none";
+  }
+  }
+  
+  //FUNCION ICONOS NAVBAR//
